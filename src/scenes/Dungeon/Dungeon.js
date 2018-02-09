e6efa6356db76331080743e0ea03107e89dde6d9
Nergi <nergi.r@gmail.com>
2018-02-09 16:36:35 +0700

    Services

--- src/scenes/Dungeon/Dungeon.js
+++ src/scenes/Dungeon/Dungeon.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import {
-    View,
+    View, Text, StyleSheet
 } from 'react-native';
+import { BLACK_COLOR, WHITE_COLOR } from '../../ColorHexa';
+import LastAttack from './LastAttack';
 
 export default class Dungeon extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     static navigationOptions = ({navigation}) => ({
-        title: 'Test',
+        title: 'Dungeon',
+        headerStyle: {
+            backgroundColor: BLACK_COLOR,
+            elevation: 0
+        },
+        headerTitleStyle: {
+            color: WHITE_COLOR
+        }
     })
 
-    render(){
-        return(
-            <View>
+    render() {
+        const { container, lastAttackContainer, textStyle } = styles;
 
+        return(
+            <View style={container}>
+                <View style={lastAttackContainer}>
+                    <Text style={textStyle}>Last Attack</Text>
+                    <LastAttack />
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    lastAttackContainer: {
+        backgroundColor: BLACK_COLOR,
+        flexDirection: 'row'
+    },
+    textStyle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        color: WHITE_COLOR,
+        marginLeft: 15,
+    }
+});
--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -9,3 +9,40 @@ export const login = (email, password) => {
                 .catch((error) => { throw error; });
         });
 }
+
+export const fetchUser = (callback) => {
+    firebase.auth().onAuthStateChanged((user) => {
+        return firebase.database().ref(`/users/${user.uid}`).on('value', (snapshot) => {
+            let user = snapshot.val();
+            let promises = [];
+            for(let id in user.weapons) {
+                promises.push(firebase.database().ref(`/weapons/${id}`).once('value'));
+            }
+            for(let id in user.potions) {
+                promises.push(firebase.database().ref(`/potions/${id}`).once('value'));
+            }
+
+            return Promise.all(promises).then((snapshot) => {
+                let weapons = [];
+                let potions = [];
+                let index = 0;
+                for(let id in user.weapons) {
+                    weapons.push(snapshot[index].val());
+                    weapons[index].count = user.weapons[id];
+                    index++;
+                }
+                index = 0;
+                for(let id in user.potions) {
+                    potions.push(snapshot[weapons.length + index].val());
+                    potions[index].count = user.potions[id];
+                    index++;
+                }
+                user.weapons = weapons;
+                user.potions = potions;
+                console.log(user);
+                return user;
+            })
+            .then((user) => callback(user));
+        });
+    })
+}
