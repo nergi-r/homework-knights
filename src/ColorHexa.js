41e1f6a0166ffc56f67eba94bf2598658d04a370
Albert Darmawan <darmawanalbert@gmail.com>
2018-02-09 14:15:02 +0700

    Adding assets and color hexa

---
+++ src/ColorHexa.js
@@ -0,0 +1,7 @@
+export const WHITE_COLOR = '#FFFFFF';
+export const BLACK_COLOR = '#2C3E50';
+export const RED_COLOR = '#E74C3C';
+export const PURPLE_COLOR = '#8E44AD';
+export const GREEN_COLOR = '#1ABC9C'; 
+export const BLUE_COLOR = '#3498DB';
+export const PINK_COLOR = '#EE2560';
