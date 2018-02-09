8fd8b1853b0d9a2361fbe4c66b25ffc890c8380e
Nergi <nergi.r@gmail.com>
2018-02-09 14:41:51 +0700

    A

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainApp from './src/App';
+import firebase from 'firebase';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyAVushE_vgB5dyWG0iAsZGKvjKrAoq25vE",
+      authDomain: "homework-knights.firebaseapp.com",
+      databaseURL: "https://homework-knights.firebaseio.com",
+      projectId: "homework-knights",
+      storageBucket: "homework-knights.appspot.com",
+      messagingSenderId: "737938770626"
+    };
+
+    if (!firebase.apps.length) {
+        firebase.initializeApp(config);
+    }
+  }
+
   render() {
     return (
       <MainApp />
--- src/scenes/Login/Login.js
+++ src/scenes/Login/Login.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
 import {
     View,
-    Button,
+    Image,
+    StyleSheet,
+    AsyncStorage
 } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import { GREEN_COLOR } from '../../ColorHexa';
 
 export default class Login extends Component {
     static navigationOptions = ({navigation}) => ({
-        title: 'Test',
+        title: 'Login',
+        header: null
     })
 
+    componentDidMount() {
+        console.ignoredYellowBox = [
+          'Setting a timer'
+        ];
+        this.setState({ isLoading: true });
+        AsyncStorage.getItem('store:auth')
+            .then((auth) => {
+                if(auth !== null) return this._goToHome();
+                this.setState({ loading: false });
+            })
+    }
+
+    _goToHome = () => {
+        const resetAction = NavigationActions.reset({
+            index: 0,
+            actions: [
+                NavigationActions.navigate({ routeName: 'home'})
+            ]
+        });
+        this.props.navigation.dispatch(resetAction);
+    }
+
     render(){
+        const { container, logoContainer } = styles;
         const { navigate } = this.props.navigation;
+
         return(
-            <View>
-                <Button
-                    onPress={() => navigate('Main')}
-                    title='Go' />
+            <View style={container}>
+                <Image source={require('../../assets/splash-screen/splash-logo.png')}
+                        style={logoContainer} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: GREEN_COLOR,
+        justifyContent: 'center'
+    },
+    logoContainer: {
+        height: 200,
+        width: 200,
+        alignSelf: 'center',
+        resizeMode: 'contain',
+    }
+});
