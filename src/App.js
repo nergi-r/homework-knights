6058e643f6b47d6fdedde3834026074b5dbbe30e
Sena Candra MPC <senacandrampc@gmail.com>
2018-02-09 14:21:53 +0700

    Structures

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { StackNavigator,  TabNavigator, TabBarBottom } from 'react-navigation';
+import Login from './scenes/Login/Login';
+import Dungeon from './scenes/Dungeon/Dungeon';
+import Home from './scenes/Home/Home';
+import Leaderboard from './scenes/Leaderboard/Leaderboard';
+import Challenge from './scenes/Challenge/Challenge';
+import Shop from './scenes/Shop/Shop';
+
+const Tab = TabNavigator({
+    Home: {
+        screen: Home,
+    },
+    Shop: {
+        screen: Shop,
+    },
+    Challenge: {
+        screen: Challenge,
+    },
+    Dungeon: {
+        screen: Dungeon,
+    },
+    Leaderboard: {
+        screen: Leaderboard,
+    },
+},  {
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    swipeEnabled: false,
+    lazy: true,
+    animationEnabled: true,
+})
+
+const Stack = StackNavigator({
+    Login: {
+        screen: Login,
+    },
+    Main: {
+        screen: Tab,
+    },
+})
+
+export default Stack;
